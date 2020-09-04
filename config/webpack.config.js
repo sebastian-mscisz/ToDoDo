ea52247c6b63312075c601ca602454786d6ae874
sebastian-mscisz <69245075+sebastian-mscisz@users.noreply.github.com>
2020-09-04 20:18:02 +0200

    Initial commit

---
+++ config/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/components/index.js",
+  },
+  output: {
+    filename: "js/[name]-[contentHash:6].js",
+    path: path.resolve(__dirname, "../", "build"),
+    publicPath: "/",
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name]-[contentHash:6].css",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/templates/index.html",
+    }),
+  ],
+};
---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return console.log("AYAYA");
+};
+
+export default App;
