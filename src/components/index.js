7452b55a65a51b57b03b29d414619e9ff0943a89
sebastian-mscisz <69245075+sebastian-mscisz@users.noreply.github.com>
2020-10-08 18:14:24 +0200

    Updates

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ToDoDo from "./ToDoDo";
-import "../sass/index.scss";
-import "typeface-inter";
-import "@fortawesome/fontawesome-free/js/all.js";
-import "@fortawesome/fontawesome-free/css/all.css";
-
-ReactDOM.render(<ToDoDo />, document.getElementById("root"));
